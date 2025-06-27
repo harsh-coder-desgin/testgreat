ff2cc4ffbf993036ceae9fd8999063d77850b3cf
harsh-coder-desgin <harshpatel51057@gmail.com>
2025-06-27 16:23:38 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,8 @@
 import { asyncHandler  } from "../utils/asyncHandler.js";
 import { ApiError} from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
-import {uploadOnCloundinary} from "../utils/cloundinary.js"
+// import {uploadOnCloundinary} from "../utils/cloundinary.js"
+import {uploadOnCloudinary} from "../utils/cloundinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
 import mongoose from "mongoose";

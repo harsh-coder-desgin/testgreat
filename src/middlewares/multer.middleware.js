2fa8f932bb2847afcd22357a09b9642c8059ed7c
harsh-coder-desgin <harshpatel51057@gmail.com>
2025-06-27 16:15:26 +0530

    Update multer.middleware.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,7 +1,26 @@
-import multer from "multer"
+import multer from "multer";
 
-const storage = multer.memoryStorage() // 🧠 Store file in memory
-
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)
+      console.log(file);
+      
+    }
+  })
+  
 export const upload = multer({ 
-    storage,
+    storage, 
 })
+
+
+// import multer from "multer"
+
+// const storage = multer.memoryStorage() // 🧠 Store file in memory
+
+// export const upload = multer({ 
+//     storage,
+// })
